3661153ecac40cf205b4e445cb8078e54b8d7606
EkaterinaTokar <ekaterinatokar19@gmail.com>
2023-07-31 23:26:16 +0500

    fix:fixed build

--- progect-management-app/src/app/app.module.ts
+++ progect-management-app/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button'
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatIconModule} from "@angular/material/icon";
 import {MatInputModule} from "@angular/material/input";
 import {ReactiveFormsModule} from "@angular/forms";
-import { FormsModule } from '@angular/forms'
+import { FormsModule} from "@angular/forms";
 import { HttpClientModule, HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http'
 import {MatDialogModule} from "@angular/material/dialog";
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
